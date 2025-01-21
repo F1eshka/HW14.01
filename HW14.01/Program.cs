a5eb16ca622f674c498e3e9fe27d4885d6e7bdb9
F1eshka <144143888+F1eshka@users.noreply.github.com>
2025-01-21 17:01:10 +0300

    Add files via upload

---
+++ HW14.01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class Program
+{
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    private static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+    static void Main()
+    {
+        MessageBox(IntPtr.Zero, "Мене звати Маліка", "Інформація про мене", 0);
+        MessageBox(IntPtr.Zero, "Мені 18 років", "Інформація про мене", 0);
+        MessageBox(IntPtr.Zero, "Я навчаюсь програмуванню та паралельно працюю веб-дизайнером та верстальником на конструкторах", "Інформація про мене", 0);
+    }
+}
